4f12c6cd960b4635bce0a83f920cfcacf5b0ca5f
Yana Radeva <yana.radeva7@gmail.com>
2024-05-20 14:16:50 +0300

    use handlebars templating engine with a template layout

--- expressJS/routes/admin.js
+++ expressJS/routes/admin.js
@@ -9,9 +9,15 @@ const router = express.Router();
 const products = [];
 
 // Route handling middlewares
-router.get('/add-product', (req, res, next) => {
-    //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
+router.get("/add-product", (req, res, next) => {
+  //res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    productCSS: true, 
+    formsCSS: true,
+    activeAddProduct: true
+  });
 });
 
 router.post('/add-product', (req, res, next) => {
