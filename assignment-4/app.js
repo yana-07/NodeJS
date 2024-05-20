bb22340b134a3d4f98cafc630c26ac9d51ffb6d1
Yana Radeva <yana.radeva7@gmail.com>
2024-05-20 16:54:10 +0300

    assignment solution using handlebars templating engine

--- assignment-4/app.js
+++ assignment-4/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
 const mainRoutes = require('./routes/index');
 const usersRoutes = require('./routes/users');
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.engine(
+  'hbs',
+  expressHbs({
+    defaultLayout: 'main-layout',
+    //default folder - does not need to be set
+    layoutsDir: 'views/layouts/',
+    // has to be set for recognition of the layout if we are not using the default extension of handlebars
+    extname: 'hbs',
+  })
+);
+
+app.set('view engine', 'hbs');
+//app.set('view engine', 'pug');
 // can be omitted, since 'views' is the default folder
 app.set('views', 'views');
 
--- assignment-4/routes/users.js
+++ assignment-4/routes/users.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const users = [];
 
 router.get('/users', (req, res, next) => {
-    res.render('users', { pageTitle: 'Users',  users });
+    res.render('users', { pageTitle: 'Users',  users, hasUsers: users.length > 0 });
 });
 
 router.post('/add-user', (req, res, next) => {
