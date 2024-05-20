17cfbf4e06a1efc636dd215634a940477ffff78d
Yana Radeva <yana.radeva7@gmail.com>
2024-05-20 14:48:39 +0300

    use ejs templating engine

--- expressJS/app.js
+++ expressJS/app.js
@@ -2,23 +2,24 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+//const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs',
+//   })
+// );
+//app.set('view engine', 'hbs');
 //app.set('view engine', 'pug');
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs')
 app.set('views', 'views');
 
 // Body parsing middleware
